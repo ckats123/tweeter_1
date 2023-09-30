c9f296a7de974c7408dc9d912c3e1663dbe0a8dc
Sophia <chiaki.katsuno@live.ca>
2023-09-30 11:36:42 -0700

    form submission using jquery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,34 +4,39 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
-// Fake data taken from initial-tweets.json
 $(document).ready(function () {
+  $("#new-tweet-form").submit(function (event) {
+    event.preventDefault();
+    console.log("Hello");
+    // Serialize the form data
+    let formData = $(this).serialize();
+    console.log(formData);
+  });
+});
 const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
+  {
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
     },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
+    content: {
+      text: "If I have seen further it is by standing on the shoulders of giants",
+    },
+    created_at: 1461116232227,
+  },
+  {
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd",
+    },
+    content: {
+      text: "Je pense , donc je suis",
+    },
+    created_at: 1461113959088,
+  },
+];
 
 const renderTweets = function (tweets) {
   const $tweetsContainer = $(".tweet-container");
@@ -41,7 +46,7 @@ const renderTweets = function (tweets) {
 
   // Loop through tweets first, then append to container
   tweets.forEach(function (tweet) {
-    console.log("tweet", tweet)
+    console.log("tweet", tweet);
     const $tweet = createTweetElement(tweet);
     $tweetsContainer.append($tweet);
   });
@@ -79,6 +84,4 @@ const createTweetElement = function (tweet) {
   return $tweet;
 };
 
-
 renderTweets(data);
-})
