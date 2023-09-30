64bddccdb5777f12d9dd16108178885545908bce
Sophia <chiaki.katsuno@live.ca>
2023-09-30 11:52:36 -0700

    fetching tweets with ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,36 +7,43 @@
 $(document).ready(function () {
   $("#new-tweet-form").submit(function (event) {
     event.preventDefault();
-    console.log("Hello");
+
     // Serialize the form data
     let formData = $(this).serialize();
-    console.log(formData);
   });
+
+  const loadTweets = function () {
+    $.get("/tweets/", function (newTweet) {
+      renderTweets(newTweet);
+    });
+  };
+
+  loadTweets();
 });
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
-];
+// const data = [
+//   {
+//     user: {
+//       name: "Newton",
+//       avatars: "https://i.imgur.com/73hZDYK.png",
+//       handle: "@SirIsaac",
+//     },
+//     content: {
+//       text: "If I have seen further it is by standing on the shoulders of giants",
+//     },
+//     created_at: 1461116232227,
+//   },
+//   {
+//     user: {
+//       name: "Descartes",
+//       avatars: "https://i.imgur.com/nlhLi3I.png",
+//       handle: "@rd",
+//     },
+//     content: {
+//       text: "Je pense , donc je suis",
+//     },
+//     created_at: 1461113959088,
+//   },
+// ];
 
 const renderTweets = function (tweets) {
   const $tweetsContainer = $(".tweet-container");
@@ -48,6 +55,9 @@ const renderTweets = function (tweets) {
   tweets.forEach(function (tweet) {
     console.log("tweet", tweet);
     const $tweet = createTweetElement(tweet);
+    // Use timeago to display time since tweet creation
+    const $timeAgo = $("<p>").text(timeago.format(tweet.created_at));
+    $tweet.find("footer").append($timeAgo);
     $tweetsContainer.append($tweet);
   });
 };
